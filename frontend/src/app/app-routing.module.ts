6ad6fdbd100e94f2c0be7f315c5776ea5f37475f
Mateus Damasceno <mtsdamasceno97@gmail.com>
2023-05-10 18:31:08 -0300

    inserindo rotas

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
+import { ListaMesesComponent } from './views/home/lista-meses/lista-meses.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
-  }
+  },
+  {
+    path: 'controle',
+    component: ListaMesesComponent
+  },
+
 ];
 
 @NgModule({
